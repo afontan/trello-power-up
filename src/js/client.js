7312909fcbe64caf25fddc5e8b1d02ba963c6de7
afontan <alejandro.fontan@mercadolibre.com>
2020-09-09 01:32:38 -0300

    change card

--- src/js/client.js
+++ src/js/client.js
@@ -6,17 +6,16 @@ var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9
 
 TrelloPowerUp.initialize({
     'card-badges': function (t, opts) {
-        return t.card('name')
-            .get('name')
-            .then(function(cardName){
-                console.log('We just loaded the card name for fun222: ' + cardName);
+        return t.card('all')
+            .then(function(card){
+                console.log(card);
                 return [{
                     // It's best to use static badges unless you need your
                     // badges to refresh.
                     // You can mix and match between static and dynamic
-                    text: 'Lu, si estas leyendo esto me debes un beso',
+                    text: 'Text',
                     icon: BLACK_ROCKET_ICON, // for card front badges only
-                    color: null
+                    color: 'orange'
                 }];
             });
     }
